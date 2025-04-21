080e536e9f53f0b0f9190e99986db351e3dce6ea
Stephan Raabe <mail@stephan-raabe.de>
2025-04-21 17:30:48 +0200

    Add matugen

--- share/dotfiles/.config/nwg-dock-hyprland/launch.sh
+++ share/dotfiles/.config/nwg-dock-hyprland/launch.sh
@@ -5,9 +5,12 @@
 #
 running=$(pgrep -c nwg-dock-hyprla)
 
+
 config="$HOME/.config/gtk-3.0/settings.ini"
 
 if [ ! -f $HOME/.config/ml4w/settings/dock-disabled ]; then
+    killall nwg-dock-hyprland
+    sleep 0.5
     prefer_dark_theme="$(grep 'gtk-application-prefer-dark-theme' "$config" | sed 's/.*\s*=\s*//')"
     if [ $prefer_dark_theme == 0 ]; then
         style="style-light.css"
